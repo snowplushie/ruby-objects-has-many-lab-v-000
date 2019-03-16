e95c3fe0d05d4aa1ba8b31d3c4e113d1907344a4
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:10:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,12 +18,14 @@ class Artist
   
   def add_song_by_name(name)
     song = Song.new(name)
-     @songs << song
     song.artist = self
-    @@song_count += 1
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
   
   def self.song_count
-    @@song_count
+    Song.all.count
   end
 end
