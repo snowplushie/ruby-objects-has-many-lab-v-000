806130acac3f7018b3c28b2c11b2bf4f8eeb45ce
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:14:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,31 @@
 class Artist
   attr_accessor :name
   
-  @@all = []
+  @@songs = []
   
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  
-  def self.all
-    @@all
+    @songs = []
   end
   
   def add_song(song)
-    song.artist = self
-  end
-  
-  def add_song_by_name(name)
-    song = Song.new(name)
+    @songs << song
+    @@songs << song
     song.artist = self
   end
   
   def songs
-    Song.all.select {|song| song.artist == self}
+    @songs
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    @@songs << song
+    song.artist = self
   end
   
   def self.song_count
-    Song.all.count
+    @@songs.count
   end
 end
