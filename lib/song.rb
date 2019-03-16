4504f86ed92aa54c22706cf54e772471a1ec481e
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:18:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,15 @@ class Song
     @name = name
     @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if self.artist == nil
+      nil
+      els self.artist.name
+    end
+  end
 end
