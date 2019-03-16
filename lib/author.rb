d69cbaa00c6758999202d4472a2d5d4d73af4367
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:29:29 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,30 @@
 class Author
   attr_accessor :name, :posts
-  @@posts = []
+  @@post_count = 0
   
   def initialize(name)
     @name = name
-    @@posts = []
+    @posts = []
+  end
+  
+  def self.post_count
+    @@post_count
   end
   
   def add_post(post)
+    @posts << post
     post.author = self
-    @post << post
-    @@posts << post
+    @@post_count +=1
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count +=1
+  end
+  
+  def posts
+    @posts
   end
 end
