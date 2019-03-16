cadac2cb79b72a4939b392c83c1c958f4532f9fa
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:25:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,15 @@
 class Author
+  attr_accessor :name, :posts
+  @@posts = []
+  
+  def initialize(name)
+    @name = name
+    @@posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+    @post << post
+    @@posts << post
+  end
 end
