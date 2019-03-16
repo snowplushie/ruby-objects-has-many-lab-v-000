51f45e5bdfdf458fb4c34fd009cf8ed0f658af58
Paige Wiener <snowplushie@yahoo.com>
2019-03-16 04:03:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,10 @@ class Artist
     @@song_count += 1
   end
   
-  def add_song_by_name(song)
-    
+  def add_song_by_name(name)
+    song = Song.new(name)
+     @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 end
